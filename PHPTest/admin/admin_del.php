f7696de48e226e8be17a21ef3d74b8bef2b45c54
baobao.kang <baobao.kang@godinsec.com>
2017-09-27 11:45:44 +0800

    提交代码完善了管理员管理界面

--- PHPTest/admin/admin_del.php
+++ PHPTest/admin/admin_del.php
@@ -5,3 +5,12 @@
  * Date: 2017/9/26
  * Time: 下午5:08
  */
+include "../conn.php";
+include "islogin.php";
+$id=$_GET['id'];
+$result=mysqli_query($mysql,"delete from admin where id=$id");
+if($result){
+    echo "<script>alert('删除成功');location.href='admin.php'</script>";
+}else{
+    echo "<script>alert('删除失败');location.href='admin.php'</script>";
+}
